c9534d7b57cde05961a6ae79478a088a5e028f0b
yusufyilmaz00 <yusufyyilmaz0@gmail.comm>
2022-12-23 02:23:54 +0300

    first commit

---
+++ game.c
@@ -0,0 +1,121 @@
+// yýlan[tahta boyutu][1][1] = uzunluk,x kor., y kor
+
+// son yemeði yediðinde boyutu 1 arttýrýp ya da o halini ekrana yazdýrabilirsin.
+
+// boyut 10 olsun. son yemeði yediðinde 11 de bastýrabilirsin 10 da
+
+// yazýlarý renklendirmek için kutuphane kullanabilrisin
+
+// kendin fonk. yaz
+
+
+// yýlan (1-n) , yemekler 0, diðerleri boþluk
+
+// kuyruk büyüyecek ( büyüme bir sonraki hamlede gerçekleþecek)
+
+
+
+
+	
+
+	
+// her adýmda tahta güncellenmeli ve hamle sayýsý gösterilmeli ( + puan da olabilir)
+
+// yönler U,D,L,R
+
+// oyun tamamlanýnca kaç hamlede tüm meyveleri yedi, yazdýr
+
+// duvara çarpýnca oyun sonlanmalý , kaybettiniz mesajý:
+	
+	// hamle sayýsý, yýlan büyüklüðü ve geriye kalan yemek sayýsý yazdýr.
+	
+// yýlan yemek yediði kareye gelince deðil, hareket edince büyümeli
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// rand() % 6 + 1; ( %6 bitiþ deðeri, +x = kaydýrma deðeri)[1,6]
+
+int main(){
+	// tahta boyutlarý input ( M X N )
+	int M,N,yemek_sayisi,yemek_x,yemek_y,i,j,yilan_x,yilan_y,game;
+	char hareket;
+	
+	printf("Tahta boyutlarini M x N seklinde giriniz:\n");
+	scanf("%d",&M);	
+	scanf("%d",&N);
+	
+	// yemek sayýsý input :
+	printf("Yemek sayisini giriniz (Tahta boyutundan fazla olamaz!\n)");
+	scanf("%d",&yemek_sayisi);
+	
+	// tahtaya sýðan yemek sayýsý kontrol edilmeli, gerekirse tekrar alýnmalý
+	while(yemek_sayisi >= (M*N) ){
+		if(yemek_sayisi >= (M*N)){
+		printf("Daha kucuk bir sayi giriniz !!\n");
+		scanf("%d",&yemek_sayisi);
+		}
+	}
+	
+	// oyun tahtasý oluþturalým
+	char tablo[M][N];
+	
+	for(i=0;i<M;i++){
+		for(j=0;j<N;j++){
+			tablo[i][j]='_';
+		}
+	}
+	
+	// olasý bir hata:
+			// içinde zaten 0 olan bir hücreye 0 tekrar 0 koyup daha az meyve yerleþtirebilir, diklkat et. if ile içini kontrol ettir.
+	// yemek pozisyonu
+	i= yemek_sayisi;
+	while( i>0 ){
+		yemek_x= rand() % M;
+		yemek_y= rand() % N;
+		
+		if(tablo[yemek_x][yemek_y] =='_'){
+			tablo[yemek_x][yemek_y]='0';	
+			i -= 1;
+		}
+	}
+	// yýlan baþlangýç pozisyonu
+	j=1;
+	while(j != 0){
+		yilan_x= rand() % M;
+		yilan_y= rand() % N;
+		
+		if(tablo[yilan_x][yilan_y] =='_' ){
+			tablo[yilan_x][yilan_y]='1';	
+			j -= 1;
+		}
+	}
+	
+	// tabloyu bastýralým
+	for(i=0;i<M;i++){
+		for(j=0;j<N;j++){
+			printf("%c ",tablo[i][j]);
+		}
+		printf("\n");
+	}
+
+	game = 1;
+	while(game == 1){
+		//process
+		printf("Hareket yonunu gir: ");
+		scanf("%s",&hareket);
+		
+		if(hareket=='u' || hareket=='d' || hareket=='l' || hareket=='r'){
+			//oyun devam
+			printf("correct\n");
+		}
+		else{
+			printf("Hatali tus basimi,Yeni ");
+		}
+	}
+	
+	
+	return 0;
+	
+}                   
+
---
+++ snake_move.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(){
+	int M,N,i,j,yilan_x,yilan_y;
+	
+	/*
+	printf("Tahta boyutlarini M x N seklinde giriniz:\n");
+	scanf("%d",&M);	
+	scanf("%d",&N);
+	*/
+	M=7;
+	N=7;
+	
+	// tablo oluþtur
+	char tablo[M][N];
+	
+	for(i=0;i<M;i++){
+		for(j=0;j<N;j++){
+			tablo[i][j]='_';
+		}
+	}
+	
+	// yýlan koordinatlarý
+	int yilan[M*N][3];
+	int max_boyut;
+	max_boyut=1;
+	
+	// yýlaný rastgele canlandýr
+	srand(time(NULL));
+	j=1;
+	while(j != 0){
+		yilan_x= rand() % M;
+		yilan_y= rand() % N;
+		
+		if(tablo[yilan_x][yilan_y] =='_' ){
+			//tablo[yilan_x][yilan_y]='1';
+				
+			yilan[max_boyut-1][0] = max_boyut;
+			yilan[max_boyut-1][1] = yilan_x;
+			yilan[max_boyut-1][2] = yilan_y;
+			j -= 1;
+		}
+	}
+	printf("rastgele\n");
+	char game='T';
+	char hareket;
+	int x_degis,y_degis;
+	
+	
+	while(game=='T'){
+		printf(" test 0 :%c\n ",game);
+		// tabloyu güncelle;
+		for(i=0;i<max_boyut;i++){
+			for(j=0;j<3;j++){
+				if( yilan[i][0]>0){	
+					yilan_x= yilan[i][1];
+					yilan_y= yilan[i][2];
+					tablo[yilan_x][yilan_y] = (char) yilan[i][0];
+				}
+			}
+		}
+		printf(" test 1 :%c\n ",game);
+		printf("tabloyu bas\n");
+		
+		for(i=0;i<M;i++){
+			for(j=0;j<N;j++){
+				printf("%c ",tablo[i][j]);
+			}
+			printf("\n");
+		}
+		printf(" test 2 :%c\n ",game);
+		//process
+		printf("Hareket yonunu gir: ");
+		scanf("%s",&hareket);
+		
+		if(hareket=='u' || hareket=='d' || hareket=='l' || hareket=='r'){
+			//oyun devam
+			if(hareket=='u'){
+				x_degis = 0;
+				y_degis = 1;
+				game = 'T';
+				printf(" test up :%c\n ",game);
+				printf("yukari\n");
+			}
+			else if(hareket=='d'){
+				x_degis = 0;
+				y_degis = -1;
+				printf(" test down :%c\n ",game);
+				printf("asagi\n");
+			}
+			else if(hareket=='l'){
+				x_degis = -1;
+				y_degis = 0;
+				printf(" test left :%c\n ",game);
+				printf("sol\n");
+			}
+			else if(hareket=='r'){
+				x_degis = 1;
+				y_degis = 0;
+				printf(" test right :%c\n ",game);
+				printf("sag\n");
+			}
+			
+			// yýlanýn koordinatlarýný güncelle
+			for(i=0;i<max_boyut;i++){
+					yilan[i][1] += x_degis;
+					yilan[i][2] += y_degis;
+				}
+			printf("yilan da guncel\n");	
+			// if bitiren poarantez	
+			}
+		else{
+			printf("Hatali tus basimi,Yeni ");
+		}
+		perror("bnvnbv");
+		printf("%d test 0 : ",game);
+		//oyun bitiren parantez
+	}
+	
+	
+	
+	return 0;
+}
